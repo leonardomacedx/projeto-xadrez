92ce43b1980eb9ebae8e6e2b4b84e4868931c812
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-04-26 11:19:37 -0300

    Implementando classes Board e Piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,11 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(2, 2);
-        System.out.println(position);
+
+        Board board = new Board(8, 8);
     }
 }
