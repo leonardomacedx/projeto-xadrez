32ba9fa0c09927891fd3957b0431e3b7d5ea153c
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-05-16 11:03:37 -0300

    Movimentos possiveis de uma peça

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ import chess.ChessPosition;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
